c842a4360d10ae84ab2b837264de6fb5da1f112c
volksvargas <varguizm@gmail.com>
2021-11-26 15:44:54 +0000

    added plugins prop

--- src/Base.svelte
+++ src/Base.svelte
@@ -13,24 +13,29 @@
     yMarkers: {},
     yRegions: [],
   };
+
   export let type = 'line';
   export let options = {};
+  export let plugins = [];
   let chart = null;
   let chartRef;
   let props = clean($$props, ["data", "type", "options"]);
+
   onMount(() => {
     chart = new Chart(chartRef, {
       type,
       data,
-      options
+      options,
+      plugins
     });
   });
+  
   afterUpdate(() => {
     if (!chart) return;
-
     chart.data = data;
     chart.type = type;
     chart.options = options;
+    chart.plugins = plugins
     chart.update()
   });
 
